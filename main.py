34523ab787195e6ea95943b37655dc9202fb01bf
n3nt0 <tnoto3Egmail.com>
2015-03-01 22:23:32 -0500

    Add response

--- main.py
+++ main.py
@@ -11,21 +11,21 @@ import os
 #os.chdir('modules/'+sys.argv[0])
 os.chdir(sys.argv[0][:-7])
 
-#Read list of carriers and format a dictionary object to choose email suffix from
+# Read list of carriers and format a dictionary object to choose email suffix from
 with open('carrierdb.csv', mode='r') as infile:
     reader = csv.reader(infile)
     with open('carrierdb_new.csv', mode='w') as outfile:
         writer = csv.writer(outfile)
         carriers = {rows[0]: rows[1] for rows in reader}
 
-#Make the list of carriers uppercase to avoid conflict with user input
+# Make the list of carriers uppercase to avoid conflict with user input
 carriers_upper = {}
 upper=''
 for i in carriers:
     #upper = i.upper()
     carriers_upper[i.upper()] = carriers[i]
 
-#Determine and format the final target address
+# Determine and format the final target address
 if sys.argv[2].upper() == 'EMAIL':
     target = sys.argv[1]
 else:
@@ -34,12 +34,12 @@ else:
 
 amount = sys.argv[3]
 
-#READ FROM TEXT FILE IN FUTURE
+# READ FROM TEXT FILE IN FUTURE
 user = 'n3tn0.chariot@gmail.com'
 password = 'chariottest'
 smtpserver = 'smtp.gmail.com:587'
 
-#Get the message to be sent
+# Get the message to be sent
 body = sys.argv[4]
 
 #Prepare message for sending
@@ -52,15 +52,20 @@ try:
 except:
     exit()
 
-#Login to the email server
+# Login to the email server
 try:
     server.login(user, password)
 except smtplib.SMTPAuthenticationError:
     exit()
 
-#Send the message
+# Send the message
 for i in range(int(amount)+1):
     server.sendmail(user, target, body)
 
+#Write response
+response = open('../response.txt')
+response.write(sys.argv[1] + 'has been oppressed.')
+
 #Cleanup
 server.quit()
+response.close()
